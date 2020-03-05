9edd31e80f0039fd19d36dfc6cdd45c70188a1f1
Marcin Tojek <marcin.tojek@elastic.co>
2020-03-05 15:40:39 +0100

    Regenerate packages

--- dev/import-beats/packages.go
+++ dev/import-beats/packages.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
@@ -16,22 +17,54 @@ import (
 	"github.com/elastic/package-registry/util"
 )
 
-var ignoredModules = map[string]bool{ "apache2": true }
+var ignoredModules = map[string]bool{"apache2": true}
 
-type packageMap map[string]*util.Package
+type packageContent struct {
+	manifest util.Package
+	datasets map[string]datasetContent
+}
 
-func (p packageMap) loadFromSource(beatsDir, beatName, packageType string) error {
+func newPackageContent(name string) packageContent {
+	title := strings.Title(name)
+	return packageContent{
+		manifest: util.Package{
+			FormatVersion: "1.0.0",
+			Name:          name,
+			Description:   strings.Title(name + " integration"),
+			Title:         &title,
+			Version:       "0.0.1", // TODO
+			Type:          "integration",
+			License:       "basic",
+		},
+		datasets: map[string]datasetContent{},
+	}
+}
+
+type datasetContent struct {
+	fields fieldsContent
+}
+
+type fieldsContent struct {
+	files map[string]fields
+}
+
+type packageRepository struct {
+	packages map[string]packageContent
+}
+
+func newPackageRepository() *packageRepository {
+	return &packageRepository{
+		packages: map[string]packageContent{},
+	}
+}
+
+func (r *packageRepository) loadFromSource(beatsDir, beatName, packageType string) error {
 	beatPath := filepath.Join(beatsDir, beatName)
 	beatModulesPath := filepath.Join(beatPath, "module")
 
 	moduleDirs, err := ioutil.ReadDir(beatModulesPath)
 	if err != nil {
 		return errors.Wrapf(err, "cannot read directory '%s'", beatModulesPath)
-	}
-
-	fieldsFile, err := loadFields(beatPath)
-	if err != nil {
-		return errors.Wrapf(err, "loading fields failed")
 	}
 
 	for _, moduleDir := range moduleDirs {
@@ -45,40 +78,33 @@ func (p packageMap) loadFromSource(beatsDir, beatName, packageType string) error
 			continue
 		}
 
-		_, ok := p[moduleDir.Name()]
+		_, ok := r.packages[moduleDir.Name()]
 		if !ok {
-			p[moduleDir.Name()] = &util.Package{
-				FormatVersion: "1.0.0",
-				Name:          moduleDir.Name(),
-				Version:       "0.0.1", // TODO
-				Type:          "integration",
-				License:       "basic",
-			}
+			r.packages[moduleDir.Name()] = newPackageContent(moduleDir.Name())
 		}
 
-		fieldsEntry, err := fieldsFile.getEntry(moduleDir.Name())
-		if err != nil {
-			return errors.Wrapf(err, "retrieving fields entry failed")
-		}
-
-		p[moduleDir.Name()].Categories = append(p[moduleDir.Name()].Categories, packageType)
-		p[moduleDir.Name()].Title = fieldsEntry.Title
-		p[moduleDir.Name()].Description = fieldsEntry.Description
+		aPackage := r.packages[moduleDir.Name()]
+		manifest := aPackage.manifest
+		manifest.Categories = append(manifest.Categories, packageType)
+		aPackage.manifest = manifest
+		r.packages[moduleDir.Name()] = aPackage
 	}
 	return nil
 }
 
-func (p packageMap) writePackages(outputDir string) error {
-	for name, content := range p {
-		log.Printf("Writing package '%s' (version: %s)\n", name, content.Version)
+func (r *packageRepository) save(outputDir string) error {
+	for name, content := range r.packages {
+		manifest := content.manifest
 
-		path := filepath.Join(outputDir, name+"-"+content.Version)
+		log.Printf("Writing package '%s' (version: %s)\n", name, manifest.Version)
+
+		path := filepath.Join(outputDir, name+"-"+manifest.Version)
 		err := os.MkdirAll(path, 0755)
 		if err != nil {
 			return errors.Wrapf(err, "cannot make directory '%s'", path)
 		}
 
-		m, err := yaml.Marshal(content)
+		m, err := yaml.Marshal(content.manifest)
 		if err != nil {
 			return errors.Wrapf(err, "marshaling package content failed (package name: %s)", name)
 		}
