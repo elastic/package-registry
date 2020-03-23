43f811dcbbfbd0067f00e04e132826e24a0ec421
Marcin Tojek <mtojek@users.noreply.github.com>
2020-03-23 22:10:13 +0100

    import-beats: Add metric input vars (#289)

--- dev/import-beats/streams.go
+++ dev/import-beats/streams.go
@@ -5,9 +5,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path"
+	"path/filepath"
+	"sort"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -24,39 +28,52 @@ type varWithDefault struct {
 	Default interface{} `yaml:"default"`
 }
 
+var ignoredConfigOptions = []string{
+	"module",
+	"metricsets",
+	"enabled",
+}
+
+// createStreams method builds a set of stream inputs including configuration variables.
+// Stream defintions depend on a beat type - log or metric.
+// At the moment, the array returns only one stream.
 func createStreams(modulePath, moduleName, datasetName, beatType string) ([]util.Stream, error) {
 	switch beatType {
 	case "logs":
-		return createLogStreams(modulePath, moduleName, datasetName, beatType)
+		return createLogStreams(modulePath, moduleName, datasetName)
 	case "metrics":
-		return createMetricStreams()
+		return createMetricStreams(modulePath, moduleName, datasetName)
 	}
 	return nil, fmt.Errorf("invalid beat type: %s", beatType)
 }
 
-func createLogStreams(modulePath, moduleName, datasetName, beatType string) ([]util.Stream, error) {
-	datasetPath := path.Join(modulePath, datasetName, "manifest.yml")
-	manifestPath, err := ioutil.ReadFile(datasetPath)
+// createLogStreams method builds a set of stream inputs for logs oriented dataset.
+// The method unmarshals "manifest.yml" file and picks all configuration variables.
+func createLogStreams(modulePath, moduleName, datasetName string) ([]util.Stream, error) {
+	manifestPath := path.Join(modulePath, datasetName, "manifest.yml")
+	manifestFile, err := ioutil.ReadFile(manifestPath)
 	if err != nil {
-		return nil, errors.Wrapf(err, "reading manifest file failed (path: %s)", datasetPath)
+		return nil, errors.Wrapf(err, "reading manifest file failed (path: %s)", manifestPath)
 	}
 
 	var mwv manifestWithVars
-	err = yaml.Unmarshal(manifestPath, &mwv)
+	err = yaml.Unmarshal(manifestFile, &mwv)
 	if err != nil {
-		return nil, errors.Wrapf(err, "unmarshalling manifest file failed (path: %s)", datasetPath)
+		return nil, errors.Wrapf(err, "unmarshalling manifest file failed (path: %s)", manifestPath)
 	}
 
 	return []util.Stream{
 		{
 			Input:       "logs",
-			Title:       fmt.Sprintf("%s %s %s", strings.Title(moduleName), strings.Title(datasetName), beatType),
+			Title:       fmt.Sprintf("%s %s logs", strings.Title(moduleName), strings.Title(datasetName)),
 			Description: fmt.Sprintf("Collect %s %s logs", strings.Title(moduleName), strings.Title(datasetName)),
 			Vars:        wrapVariablesWithDefault(mwv).Vars,
 		},
 	}, nil
 }
 
+// wrapVariablesWithDefault method ensures that all variable values are wrapped with a "default" field, even if they are
+// defined for particular operating systems (prefix: os.)
 func wrapVariablesWithDefault(mwvs manifestWithVars) manifestWithVars {
 	var withDefaults manifestWithVars
 	for _, aVar := range mwvs.Vars {
@@ -75,10 +92,124 @@ func wrapVariablesWithDefault(mwvs manifestWithVars) manifestWithVars {
 	return withDefaults
 }
 
-func createMetricStreams() ([]util.Stream, error) {
+// wrapVariablesWithDefault method builds a set of stream inputs for metrics oriented dataset.
+// The method combines all config files in module's _meta directory, unmarshals all configuration entries and selects
+// ones related to the particular metricset (first seen, first occurrence, next occurrences skipped).
+//
+// The method skips commented variables, but keeps arrays of structures (even if it's not possible to render them using
+// UI).
+func createMetricStreams(modulePath, moduleName, datasetName string) ([]util.Stream, error) {
+	merged, err := mergeMetaConfigFiles(modulePath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "merging config files failed")
+	}
+
+	var configOptions []map[string]interface{}
+
+	if len(merged) > 0 {
+		var moduleConfig []mapStr
+		err = yaml.Unmarshal(merged, &moduleConfig)
+		if err != nil {
+			return nil, errors.Wrapf(err, "unmarshalling module config failed (moduleName: %s, datasetName: %s)",
+				moduleName, datasetName)
+		}
+
+		foundConfigEntries := map[string]bool{}
+
+		for _, moduleConfigEntry := range moduleConfig {
+			flatEntry := moduleConfigEntry.flatten()
+			related, err := isConfigEntryRelatedToMetricset(flatEntry, moduleName, datasetName)
+			if err != nil {
+				return nil, errors.Wrapf(err, "checking if config entry is related failed (moduleName: %s, datasetName: %s)",
+					moduleName, datasetName)
+			}
+			if !related {
+				continue
+			}
+
+			for name, value := range flatEntry {
+				if shouldConfigOptionBeIgnored(name) {
+					continue
+				}
+
+				if _, ok := foundConfigEntries[name]; ok {
+					continue // already processed this config option
+				}
+
+				configOptions = append(configOptions, map[string]interface{}{
+					"default": value,
+					"name":    name,
+				})
+				foundConfigEntries[name] = true
+			}
+		}
+
+		// sort variables to keep them in order while using version control.
+		sort.Slice(configOptions, func(i, j int) bool {
+			return sort.StringsAreSorted([]string{configOptions[i]["name"].(string), configOptions[j]["name"].(string)})
+		})
+	}
+
 	return []util.Stream{
 		{
-			Input: "TODO",
+			Input:       moduleName + "/metrics",
+			Title:       fmt.Sprintf("%s %s logs", strings.Title(moduleName), strings.Title(datasetName)),
+			Description: fmt.Sprintf("Collect %s %s metrics", strings.Title(moduleName), strings.Title(datasetName)),
+			Vars:        configOptions,
 		},
-	}, nil // TODO
+	}, nil
+}
+
+// mergeMetaConfigFiles method visits all configuration YAML files and combines them into single document.
+func mergeMetaConfigFiles(modulePath string) ([]byte, error) {
+	configFilePaths, err := filepath.Glob(filepath.Join(modulePath, "_meta", "config*.yml"))
+	if err != nil {
+		return nil, errors.Wrapf(err, "location config files failed (modulePath: %s)", modulePath)
+	}
+
+	var mergedConfig bytes.Buffer
+	for _, configFilePath := range configFilePaths {
+		configFile, err := ioutil.ReadFile(configFilePath)
+		if err != nil && !os.IsNotExist(err) {
+			return nil, errors.Wrapf(err, "reading config file failed (path: %s)", configFilePath)
+		}
+		mergedConfig.Write(configFile)
+		mergedConfig.WriteString("\n")
+	}
+	return mergedConfig.Bytes(), nil
+}
+
+// shouldConfigOptionBeIgnored method checks if the configuration option name should be skipped (not used, duplicate, etc.)
+func shouldConfigOptionBeIgnored(optionName string) bool {
+	for _, ignored := range ignoredConfigOptions {
+		if ignored == optionName {
+			return true
+		}
+	}
+	return false
+}
+
+// isConfigEntryRelatedToMetricset method checks if the configuration entry may affect the dataset settings,
+// in other words, checks if the "metricsets" field is present and contains the given datasetName.
+func isConfigEntryRelatedToMetricset(entry mapStr, moduleName, datasetName string) (bool, error) {
+	var metricsetRelated bool
+	if metricsets, ok := entry["metricsets"]; ok {
+		metricsetsMapped, ok := metricsets.([]interface{})
+		if !ok {
+			return false, fmt.Errorf("mapping metricsets failed (moduleName: %s, datasetName: %s)",
+				moduleName, datasetName)
+		}
+		if len(metricsetsMapped) == 0 {
+			return false, fmt.Errorf("no metricsets defined (moduleName: %s, datasetName: %s)", moduleName,
+				datasetName)
+		}
+
+		for _, metricset := range metricsetsMapped {
+			if metricset.(string) == datasetName {
+				metricsetRelated = true
+				break
+			}
+		}
+	}
+	return metricsetRelated, nil
 }
