7c1fb4a95d30f049585513c51c7b0271f9ad7720
mtojek <marcin.tojek@elastic.co>
2020-06-01 13:49:00 +0200

    Validate required fields

--- dev/generator/main.go
+++ dev/generator/main.go
@@ -13,6 +13,8 @@ import (
 	"os"
 	"path/filepath"
 
+	"gopkg.in/yaml.v2"
+
 	"github.com/magefile/mage/sh"
 	"github.com/pkg/errors"
 
@@ -45,6 +47,11 @@ const (
     Event timestamp.
 `
 )
+
+type fieldEntry struct {
+	name  string
+	aType string
+}
 
 func main() {
 	// Directory with a list of packages inside
@@ -171,15 +178,21 @@ func buildPackage(packagesBasePath string, p util.Package) error {
 		return err
 	}
 
-	// Add base-fields.yml to all dataset with the basic stream fields and @timestamp
+	// Add base-fields.yml if missing to all dataset with the basic stream fields and @timestamp
 	for _, dataset := range datasets {
-		dirPath := filepath.Join(p.BasePath, "dataset", dataset, "fields")
-		err := os.MkdirAll(dirPath, 0755)
+		datasetPath := filepath.Join(p.BasePath, "dataset", dataset)
+
+		err = createBaseFieldsFile(datasetPath)
 		if err != nil {
 			return err
 		}
+	}
 
-		err = ioutil.WriteFile(filepath.Join(dirPath, "base-fields.yml"), []byte(streamFields), 0644)
+	// Validate if basic stream fields and @timestamp are present
+	for _, dataset := range datasets {
+		datasetPath := filepath.Join(p.BasePath, "dataset", dataset)
+
+		err = validateRequiredFields(datasetPath)
 		if err != nil {
 			return err
 		}
@@ -224,6 +237,119 @@ func buildPackage(packagesBasePath string, p util.Package) error {
 		}
 	}
 	return nil
+}
+
+// createBaseFieldsFile method creates the base-fields.yml file if missing.
+func createBaseFieldsFile(datasetPath string) error {
+	fieldsDirPath := filepath.Join(datasetPath, "fields")
+	baseFieldsPath := filepath.Join(fieldsDirPath, "base-fields.yml")
+	_, err := os.Stat(baseFieldsPath)
+	if err == nil {
+		return nil
+	} else if !os.IsNotExist(err) {
+		return err
+	}
+
+	err = os.MkdirAll(fieldsDirPath, 0755)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(baseFieldsPath, []byte(streamFields), 0644)
+}
+
+// validateRequiredFields method loads fields from all files and checks if required fields are present.
+func validateRequiredFields(datasetPath string) error {
+	fieldsDirPath := filepath.Join(datasetPath, "fields")
+
+	// Collect fields from all files
+	var allFields []MapStr
+	err := filepath.Walk(fieldsDirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relativePath, err := filepath.Rel(fieldsDirPath, path)
+		if err != nil {
+			return err
+		}
+
+		if relativePath == "." {
+			return nil
+		}
+
+		body, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		var m []MapStr
+		err = yaml.Unmarshal(body, &m)
+		if err != nil {
+			return err
+		}
+
+		allFields = append(allFields, m...)
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	// Flatten all fields
+	for i, fields := range allFields {
+		allFields[i] = fields.Flatten()
+	}
+
+	// Verify required keys
+	err = requireField(allFields, "stream.type", "constant_keyword", err)
+	err = requireField(allFields, "stream.dataset", "constant_keyword", err)
+	err = requireField(allFields, "stream.namespace", "constant_keyword", err)
+	err = requireField(allFields, "@timestamp", "date", err)
+	return err
+}
+
+func requireField(allFields []MapStr, searchedName, expectedType string, validationErr error) error {
+	if validationErr != nil {
+		return validationErr
+	}
+
+	f, err := findField(allFields, searchedName)
+	if err != nil {
+		return err
+	}
+
+	if f.aType != expectedType {
+		return fmt.Errorf("wrong field type for '%s' (expected: %s, got: %s)", searchedName, expectedType, f.aType)
+	}
+	return nil
+}
+
+func findField(allFields []MapStr, searchedName string) (*fieldEntry, error) {
+	for _, fields := range allFields {
+		name, err := fields.GetValue("name")
+		if err != nil {
+			return nil, err
+		}
+
+		if name != searchedName {
+			continue
+		}
+
+		aType, err := fields.GetValue("type")
+		if err != nil {
+			return nil, err
+		}
+
+		if aType == "" {
+			return nil, fmt.Errorf("field '%s' found, but type is undefined", searchedName)
+		}
+
+		return &fieldEntry{
+			name:  name.(string),
+			aType: aType.(string),
+		}, nil
+	}
+	return nil, fmt.Errorf("field '%s' not found", searchedName)
 }
 
 var (
