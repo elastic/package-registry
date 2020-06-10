314263e5e42c588d81dd09b27a888f40442c52fc
Nicolas Ruflin <spam@ruflin.com>
2020-06-10 10:54:57 +0200

    Add validation for dataset type (#501)
    The valid dataset types at the moment are logs, metrics and events. events will be removed in the future but at the moment it is still used in the endpoint package. As soon as this is removed, the code here will be cleaned up.
    
    With recent changes to the code, the datasets were not validated anymore during build or running the registry. This is now changed by adding validate for datasets every time a package is created.

--- util/package.go
+++ util/package.go
@@ -317,7 +317,7 @@ func (p *Package) Validate() error {
 		}
 	}
 
-	return nil
+	return p.ValidateDatasets()
 }
 
 // GetDatasetPaths returns a list with the dataset paths inside this package
@@ -393,6 +393,28 @@ func (p *Package) LoadDataSets() error {
 		p.DataSets = append(p.DataSets, d)
 	}
 
+	return nil
+}
+
+// ValidateDatasets loads all datasets and with it validates them
+func (p *Package) ValidateDatasets() error {
+
+	datasetPaths, err := p.GetDatasetPaths()
+	if err != nil {
+		return err
+	}
+
+	datasetsBasePath := filepath.Join(p.BasePath, "dataset")
+
+	for _, datasetPath := range datasetPaths {
+
+		datasetBasePath := filepath.Join(datasetsBasePath, datasetPath)
+
+		_, err := NewDataset(datasetBasePath, p)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
