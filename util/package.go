e3e844d5ce91d77e8b94e9c6643b051322024046
kaiyan-sheng <kaiyan.sheng@elastic.co>
2021-03-31 23:42:28 +0800

    Add policy template icons into /search endpoint

--- util/package.go
+++ util/package.go
@@ -82,12 +82,22 @@ type BasePackage struct {
 	Path        string  `json:"path" yaml:"path,omitempty"`
 	Icons       []Image `config:"icons,omitempty" json:"icons,omitempty" yaml:"icons,omitempty"`
 	Internal    bool    `config:"internal,omitempty" json:"internal,omitempty" yaml:"internal,omitempty"`
+	BasePolicyTemplates []BasePolicyTemplate `config:"policy_templates,omitempty" json:"policy_templates,omitempty" yaml:"policy_templates,omitempty"`
+}
+
+// BasePolicyTemplate is used for the package policy templates in the /search endpoint
+type BasePolicyTemplate struct {
+	Name        string   `config:"name" json:"name" validate:"required"`
+	Title       string   `config:"title" json:"title" validate:"required"`
+	Description string   `config:"description" json:"description" validate:"required"`
+	Icons       []Image  `config:"icons,omitempty" json:"icons,omitempty" yaml:"icons,omitempty"`
 }
 
 type PolicyTemplate struct {
 	Name        string   `config:"name" json:"name" validate:"required"`
 	Title       string   `config:"title" json:"title" validate:"required"`
 	Description string   `config:"description" json:"description" validate:"required"`
+	DataStreams []string `config:"data_streams,omitempty" json:"data_streams,omitempty" yaml:"data_streams,omitempty"`
 	Inputs      []Input  `config:"inputs" json:"inputs"`
 	Multiple    *bool    `config:"multiple" json:"multiple,omitempty" yaml:"multiple,omitempty"`
 	Icons       []Image  `config:"icons,omitempty" json:"icons,omitempty" yaml:"icons,omitempty"`
@@ -164,9 +174,13 @@ func NewPackage(basePath string) (*Package, error) {
 			p.PolicyTemplates[i].Multiple = &trueValue
 		}
 
-		// Combine policy template icons with package level icons
-		p.Icons = append(p.Icons, p.PolicyTemplates[i].Icons...)
+		// Collect basic information from policy templates only when icons are
+		// available and store in /search endpoint
+		if p.PolicyTemplates[i].Icons == nil {
+			p.BasePolicyTemplates = nil
+		}
 	}
+
 	if p.Type == "" {
 		p.Type = defaultType
 	}
