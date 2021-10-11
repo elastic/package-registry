a6e358f980227aff85a8b044262ac920cc1ac2dd
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2021-10-11 15:31:39 +0200

    Fix issue with relative paths when loading data streams (#742)
    Rely on `filepath.Rel` to calculate relative paths for files inside
    packages.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Bugfixes
 
+* Fix issue with relative paths when loading data streams. [#742](https://github.com/elastic/package-registry/pull/742)
+
 ### Added
 
 ### Deprecated
--- packages/fs.go
+++ packages/fs.go
@@ -49,7 +49,11 @@ func (fs *ExtractedPackageFileSystem) Glob(pattern string) (matches []string, er
 		return
 	}
 	for i := range matches {
-		matches[i] = matches[i][len(fs.path+string(filepath.Separator)):]
+		match, err := filepath.Rel(fs.path, matches[i])
+		if err != nil {
+			return nil, fmt.Errorf("failed to obtain path under package root path (%s): %w", fs.path, err)
+		}
+		matches[i] = match
 	}
 	return
 }
