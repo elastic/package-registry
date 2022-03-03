662507001b41931da3a4e4fbb5472561d55f7fd9
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2022-03-03 19:23:55 +0100

    Add test cases

--- util/logging_test.go
+++ util/logging_test.go
@@ -15,40 +15,158 @@ import (
 )
 
 func TestCaptureZapFieldsForRequest(t *testing.T) {
-	body := "Hello!"
-	handler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte(body))
-	})
-
-	recorder := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "http://epr.elastic.co/search?package=foo&all=true", nil)
-	req.RemoteAddr = "233.252.0.252:33442"
-	require.NoError(t, err)
-
-	message, fields := captureZapFieldsForRequest(handler, recorder, req)
-	var duration int64
-	for _, f := range fields {
-		if f.Key == "event.duration" {
-			duration = f.Integer
-		}
+	cases := []struct {
+		title           string
+		method          string
+		url             string
+		remoteAddress   string
+		handler         http.Handler
+		expectedMessage string
+		expectedFields  []zap.Field
+	}{
+		{
+			title: "Normal GET",
+			handler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				w.Write([]byte("Hello!"))
+			}),
+			method:          "GET",
+			url:             "http://epr.elastic.co/search?package=foo&all=true",
+			remoteAddress:   "233.252.0.252:33442",
+			expectedMessage: "GET /search HTTP/1.1",
+			expectedFields: []zap.Field{
+				zap.Int64("http.response.code", 200),
+				zap.Int("http.response.body.bytes", 6),
+				zap.String("http.request.method", "GET"),
+				zap.String("source.address", "233.252.0.252"),
+				zap.String("source.ip", "233.252.0.252"),
+				zap.String("url.domain", "epr.elastic.co"),
+				zap.String("url.path", "/search"),
+				zap.String("url.query", "package=foo&all=true"),
+			},
+		},
+		{
+			title:           "404",
+			handler:         http.NotFoundHandler(),
+			method:          "GET",
+			url:             "http://epr.elastic.co/foo",
+			remoteAddress:   "233.252.0.252:33442",
+			expectedMessage: "GET /foo HTTP/1.1",
+			expectedFields: []zap.Field{
+				zap.Int64("http.response.code", 404),
+				zap.Int("http.response.body.bytes", 19),
+				zap.String("http.request.method", "GET"),
+				zap.String("source.address", "233.252.0.252"),
+				zap.String("source.ip", "233.252.0.252"),
+				zap.String("url.domain", "epr.elastic.co"),
+				zap.String("url.path", "/foo"),
+			},
+		},
+		{
+			title: "Empty response on 500 error",
+			handler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				w.WriteHeader(http.StatusInternalServerError)
+			}),
+			method:          "OPTIONS",
+			url:             "http://epr.elastic.co/search",
+			remoteAddress:   "233.252.0.252:33442",
+			expectedMessage: "OPTIONS /search HTTP/1.1",
+			expectedFields: []zap.Field{
+				zap.Int64("http.response.code", 500),
+				zap.Int("http.response.body.bytes", 0),
+				zap.String("http.request.method", "OPTIONS"),
+				zap.String("source.address", "233.252.0.252"),
+				zap.String("source.ip", "233.252.0.252"),
+				zap.String("url.domain", "epr.elastic.co"),
+				zap.String("url.path", "/search"),
+			},
+		},
+		{
+			title: "IPv4 address",
+			handler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				w.Write([]byte("Hello!"))
+			}),
+			method:          "GET",
+			url:             "http://233.252.0.10:8080/search?package=foo&all=true",
+			remoteAddress:   "233.252.0.252:33442",
+			expectedMessage: "GET /search HTTP/1.1",
+			expectedFields: []zap.Field{
+				zap.Int64("http.response.code", 200),
+				zap.Int("http.response.body.bytes", 6),
+				zap.String("http.request.method", "GET"),
+				zap.String("source.address", "233.252.0.252"),
+				zap.String("source.ip", "233.252.0.252"),
+				zap.String("url.domain", "233.252.0.10"),
+				zap.Int("url.port", 8080),
+				zap.String("url.path", "/search"),
+				zap.String("url.query", "package=foo&all=true"),
+			},
+		},
+		{
+			title: "IPv6 address",
+			handler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				w.Write([]byte("Hello!"))
+			}),
+			method:          "GET",
+			url:             "http://[2001:0DB8::10]/search?package=foo&all=true",
+			remoteAddress:   "[2001:0DB8::CAFE]:33442",
+			expectedMessage: "GET /search HTTP/1.1",
+			expectedFields: []zap.Field{
+				zap.Int64("http.response.code", 200),
+				zap.Int("http.response.body.bytes", 6),
+				zap.String("http.request.method", "GET"),
+				zap.String("source.address", "2001:0DB8::CAFE"),
+				zap.String("source.ip", "2001:0DB8::CAFE"),
+				zap.String("url.domain", "[2001:0DB8::10]"),
+				zap.String("url.path", "/search"),
+				zap.String("url.query", "package=foo&all=true"),
+			},
+		},
 	}
 
-	expectedMessage := "GET /search HTTP/1.1"
-	assert.Equal(t, expectedMessage, message)
+	for _, c := range cases {
+		t.Run(c.title, func(t *testing.T) {
+			req, err := http.NewRequest(c.method, c.url, nil)
+			require.NoError(t, err)
+			req.RemoteAddr = c.remoteAddress
 
-	expectedFields := []zap.Field{
-		zap.Int64("event.duration", duration),
-		zap.Int64("http.response.code", 200),
-		zap.Int("http.response.body.bytes", len(body)),
-		zap.String("http.request.method", "GET"),
-		zap.String("source.address", "233.252.0.252"),
-		zap.String("source.ip", "233.252.0.252"),
-		zap.String("url.domain", "epr.elastic.co"),
-		zap.String("url.path", "/search"),
-		zap.String("url.query", "package=foo&all=true"),
+			recorder := httptest.NewRecorder()
+			message, fields := captureZapFieldsForRequest(c.handler, recorder, req)
+
+			// Check only that event.duration is there, and drop it.
+			durationFound := false
+			for i, f := range fields {
+				if f.Key == "event.duration" {
+					fields = append(fields[:i], fields[i+1:]...)
+					durationFound = true
+				}
+			}
+			assert.True(t, durationFound, "event.duration expected")
+
+			// Check message.
+			assert.Equal(t, c.expectedMessage, message)
+
+			// Check fields.
+			for _, expected := range c.expectedFields {
+				found := false
+				for _, field := range fields {
+					if field.Key == expected.Key {
+						assert.Equal(t, expected, field, "field "+expected.Key)
+						found = true
+						break
+					}
+				}
+				assert.True(t, found, expected.Key+" not found")
+			}
+			for _, field := range fields {
+				found := false
+				for _, expected := range c.expectedFields {
+					if field.Key == expected.Key {
+						found = true
+						break
+					}
+				}
+				assert.True(t, found, field.Key+" not expected")
+			}
+		})
 	}
-	for _, expected := range expectedFields {
-		assert.Contains(t, fields, expected)
-	}
-	assert.Len(t, fields, len(expectedFields))
 }
