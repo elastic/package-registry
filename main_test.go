df97bd129e1649a6ff2446812389f57055d8f08c
mtojek <marcin.tojek@elastic.co>
2022-05-05 12:50:41 +0200

    Generate package input

--- main_test.go
+++ main_test.go
@@ -323,6 +323,7 @@ func TestPackageIndex(t *testing.T) {
 		{"/package/missing/1.0.0/", packageIndexRouterPath, "index-package-not-found.txt", packageIndexHandler},
 		{"/package/example/999.0.0/", packageIndexRouterPath, "index-package-revision-not-found.txt", packageIndexHandler},
 		{"/package/example/a.b.c/", packageIndexRouterPath, "index-package-invalid-version.txt", packageIndexHandler},
+		{"/package/sql_input/1.0.1/", packageIndexRouterPath, "sql-input-package.json", packageIndexHandler},
 	}
 
 	for _, test := range tests {
