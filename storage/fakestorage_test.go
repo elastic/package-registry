cfb805a2e78cc0903774396cde5994660f32459d
mtojek <marcin.tojek@elastic.co>
2022-05-17 17:19:47 +0200

    WIP

--- storage/fakestorage_test.go
+++ storage/fakestorage_test.go
@@ -5,6 +5,11 @@
 package storage
 
 import (
+	"cloud.google.com/go/storage"
+	"context"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,6 +18,8 @@ import (
 func TestPrepareFakeServer(t *testing.T) {
 	// given
 	indexFile := "testdata/search-index-all-1.json"
+	testIndexFile, err := os.ReadFile(indexFile)
+	require.NoErrorf(t, err, "index file should be present in testdata")
 
 	// when
 	fs := prepareFakeServer(t, indexFile)
@@ -22,4 +29,21 @@ func TestPrepareFakeServer(t *testing.T) {
 	client := fs.Client()
 	require.NotNil(t, client, "client should be initialized")
 
+	aCursor := readObject(t, client.Bucket(fakePackageStorageBucketInternal).Object(cursorStoragePath))
+	assert.Equal(t, []byte(`{"cursor":"1"}`), aCursor)
+	anIndex := readObject(t, client.Bucket(fakePackageStorageBucketInternal).Object(joinObjectPaths(v2MetadataStoragePath, "1", searchIndexAllFile)))
+	assert.Equal(t, testIndexFile, anIndex)
+	packageZip := readObject(t, client.Bucket(fakePackageStorageBucketPublic).Object(joinObjectPaths(artifactsPackagesStoragePath, "1password-1.1.1.zip")))
+	assert.NotZero(t, len(packageZip), ".zip package must have fake content")
+
+	// check few static files
+
+}
+
+func readObject(t *testing.T, handle *storage.ObjectHandle) []byte {
+	reader, err := handle.NewReader(context.Background())
+	require.NoErrorf(t, err, "can't initialize reader for object %s", handle.ObjectName())
+	content, err := io.ReadAll(reader)
+	require.NoErrorf(t, err, "io.ReadAll failed for object %s", handle.ObjectName())
+	return content
 }
