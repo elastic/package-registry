112537eca2ee13f340d21fe364e2440cc3d54d35
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2022-10-06 11:11:47 +0200

    Move compatibility code to common place, add unit tests (#892)

--- packages/packages.go
+++ packages/packages.go
@@ -312,13 +312,14 @@ func (f *Filter) Apply(ctx context.Context, packages Packages) Packages {
 	// Checks that only the most recent version of an integration is added to the list
 	var packagesList Packages
 	for _, p := range packages {
-		// Skip experimental packages if flag is not specified
+		// Skip experimental packages if flag is not specified.
 		if p.Release == ReleaseExperimental && !f.Experimental {
 			continue
 		}
 
-		// Skip prerelease packages by default
-		if p.IsPrerelease() && !f.Prerelease {
+		// Skip prerelease packages by default.
+		// Ignore condition if experimental flag is used (older Kibanas).
+		if !f.Experimental && p.IsPrerelease() && !f.Prerelease {
 			continue
 		}
 
