835a158a43a9060948c1210feab586145ecf12be
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2022-11-24 15:37:42 +0100

    Proxify requests to package-storage

--- proxymode/resolver.go
+++ proxymode/resolver.go
@@ -6,6 +6,8 @@ package proxymode
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"net/url"
 
@@ -16,25 +18,61 @@ type proxyResolver struct {
 	destinationURL url.URL
 }
 
+var acceptedHeaders = map[string]string{
+	"Content-Length": "",
+	"Content-Type":   "",
+	"Last-Modified":  "",
+	"Age":            "",
+}
+
+func (pr proxyResolver) pipeRequestProxy(w http.ResponseWriter, remotePath string) error {
+	remoteURL := pr.destinationURL.ResolveReference(&url.URL{Path: remotePath})
+
+	resp, err := http.Get(remoteURL.String())
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	for header, values := range resp.Header {
+		if len(w.Header().Values(header)) > 0 {
+			log.Printf(">>>> Filtered header (already exists) %s:%v", header, values)
+			continue
+		}
+		if _, ok := acceptedHeaders[header]; !ok {
+			log.Printf(">>>> Filtered header %s:%v", header, values)
+			continue
+		}
+		for _, value := range values {
+			log.Printf(">>>> Adding header %s:%s", header, value)
+			w.Header().Add(header, value)
+		}
+	}
+	w.WriteHeader(resp.StatusCode)
+
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (pr proxyResolver) RedirectArtifactsHandler(w http.ResponseWriter, r *http.Request, p *packages.Package) {
 	remotePath := fmt.Sprintf("/epr/package/%s-%s.zip", p.Name, p.Version)
-	anURL := pr.destinationURL.
-		ResolveReference(&url.URL{Path: remotePath})
-	http.Redirect(w, r, anURL.String(), http.StatusMovedPermanently)
+
+	pr.pipeRequestProxy(w, remotePath)
 }
 
 func (pr proxyResolver) RedirectStaticHandler(w http.ResponseWriter, r *http.Request, p *packages.Package, resourcePath string) {
 	remotePath := fmt.Sprintf("/package/%s/%s/%s", p.Name, p.Version, resourcePath)
-	staticURL := pr.destinationURL.
-		ResolveReference(&url.URL{Path: remotePath})
-	http.Redirect(w, r, staticURL.String(), http.StatusMovedPermanently)
+
+	pr.pipeRequestProxy(w, remotePath)
 }
 
 func (pr proxyResolver) RedirectSignaturesHandler(w http.ResponseWriter, r *http.Request, p *packages.Package) {
 	remotePath := fmt.Sprintf("/epr/package/%s-%s.zip.sig", p.Name, p.Version)
-	anURL := pr.destinationURL.
-		ResolveReference(&url.URL{Path: remotePath})
-	http.Redirect(w, r, anURL.String(), http.StatusMovedPermanently)
+
+	pr.pipeRequestProxy(w, remotePath)
 }
 
 var _ packages.RemoteResolver = new(proxyResolver)
