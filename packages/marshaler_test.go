e2b149a9a4f807b4b854a39b68ad4c2d50da94db
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2022-12-13 15:13:16 +0100

    Fix linting

--- packages/marshaler_test.go
+++ packages/marshaler_test.go
@@ -11,9 +11,10 @@ import (
 	"os"
 	"testing"
 
-	"github.com/elastic/package-registry/internal/util"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/package-registry/internal/util"
 )
 
 const testFile = "./testdata/marshaler/packages.json"
