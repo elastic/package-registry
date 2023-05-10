c4568d0f8c36abde97f81b01e8271121cdd0b030
Svetlana Maltseva <svetlana.maltseva@elastic.co>
2023-05-10 19:41:27 +0200

    refresh env fix

--- .buildkite/scripts/pre-install-command.ps1
+++ .buildkite/scripts/pre-install-command.ps1
@@ -1,6 +1,7 @@
 Write-Host "Install golang --"
 choco install -y golang
-
+$env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 refreshenv
 
 golang version
