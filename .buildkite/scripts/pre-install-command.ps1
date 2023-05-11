d74898cea63ad2e1e1eb3149b21884d248d0e823
Svetlana Maltseva <svetlana.maltseva@elastic.co>
2023-05-11 10:17:24 +0200

    add version

--- .buildkite/scripts/pre-install-command.ps1
+++ .buildkite/scripts/pre-install-command.ps1
@@ -1,7 +1,7 @@
 Write-Host "Install golang --"
-choco install -y golang
+choco install -y golang --version 1.20.3
 $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 refreshenv
 
-golang version
+go version
