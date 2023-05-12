65ed23239ca79c2a3c0cd3c3913ef214b20392a2
Svetlana Maltseva <svetlana.maltseva@elastic.co>
2023-05-12 14:11:50 +0200

    fix crlf

--- .buildkite/scripts/pre-install-command.ps1
+++ .buildkite/scripts/pre-install-command.ps1
@@ -1,3 +1,10 @@
+echo "--- Fixing CRLF in git checkout"
+# Forcing to checkout again all the files with a correct autocrlf.
+# Doing this here because we cannot set git clone options before.
+git config core.autocrlf input
+git rm --quiet --cached -r .
+git reset --quiet --hard
+
 Write-Host "-- Install golang --"
 choco install -y golang --version $env:SETUP_GOLANG_VERSION
 $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
