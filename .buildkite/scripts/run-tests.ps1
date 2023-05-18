06cfbfa627193e9870ad83c7c4ecd61fce957469
Svetlana Maltseva <svetlana.maltseva@elastic.co>
2023-05-18 17:45:10 +0200

    refactor: script for ps

--- .buildkite/scripts/run-tests.ps1
+++ .buildkite/scripts/run-tests.ps1
@@ -1,24 +1,38 @@
-Write-Host "-- Fixing CRLF in git checkout --"
 # Forcing to checkout again all the files with a correct autocrlf.
 # Doing this here because we cannot set git clone options before.
-git config core.autocrlf input
-git rm --quiet --cached -r .
-git reset --quiet --hard
+function fixCRLF {
+    Write-Host "-- Fixing CRLF in git checkout --"
+    git config core.autocrlf input
+    git rm --quiet --cached -r .
+    git reset --quiet --hard
+}
 
-Write-Host "-- Install golang --"
-choco install -y golang --version $env:SETUP_GOLANG_VERSION
-$env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
-Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-refreshenv
-go version
-go env
+function withGolang($version) {
+    Write-Host "-- Install golang --"
+    choco install -y golang --version $version
+    $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
+    Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+    refreshenv
+    go version
+    go env
+}
 
-Write-Host "-- Install go-junit-report --"
-go install github.com/jstemmer/go-junit-report/v2@latest
+# Install gotestsum for junit reports
+function withGoTestSum {
+    Write-Host "-- Install gotestsum --"
+    # go install github.com/jstemmer/go-junit-report/v2@latest
+    go install gotest.tools/gotestsum@latest
+}
 
-Write-Host "-- Run test, prepare junit-xml --"
-go mod download -x
-go test -v 2>&1 ./... | go-junit-report > "tests-report-win-unicode.xml"
-# Junit-Report will be used in linux enviroment.
-Get-Content tests-report-win-unicode.xml -Encoding Unicode | Set-Content -Encoding UTF8 tests-report-win.xml
-Remove-Item tests-report-win-unicode.xml
+# Run test, prepare junit-xml by gotestsum
+function goTestJUnit($output_file, $options) {
+    Write-Host "-- Run test, prepare junit-xml --"
+    go mod download -x
+    go install gotest.tools/gotestsum@latest
+    gotestsum --format testname --junitfile $output_file -- $options
+}
+
+fixCRLF
+withGolang $env:SETUP_GOLANG_VERSION
+withGoTestSum
+goTestJUnit 'tests-report-win.xml' '-v ./...'
