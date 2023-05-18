4b7ff45a17f2b2daf5261468c5d046c650a0056c
Svetlana Maltseva <svetlana.maltseva@elastic.co>
2023-05-18 09:51:14 +0200

    delete report

--- .buildkite/scripts/pre-install-command.ps1
+++ .buildkite/scripts/pre-install-command.ps1
@@ -1,4 +1,4 @@
-echo "--- Fixing CRLF in git checkout"
+Write-Host "-- Fixing CRLF in git checkout --"
 # Forcing to checkout again all the files with a correct autocrlf.
 # Doing this here because we cannot set git clone options before.
 git config core.autocrlf input
@@ -16,8 +16,10 @@ go env
 Write-Host "-- Install go-junit-report --"
 go install github.com/jstemmer/go-junit-report/v2@latest
 
-Write-Host "-- Run test --"
+Write-Host "-- Run test, prepare junit-xml --"
 go mod download -x
 go test -v 2>&1 ./... | go-junit-report > "tests-report-win-unicode.xml"
+# Junit-Report will be used in linux enviroment.
 Get-Content tests-report-win-unicode.xml -Encoding Unicode | Set-Content -Encoding UTF8 tests-report-win.xml
+Remove-Item tests-report-win-unicode.xml
 ls
