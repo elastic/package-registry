617702f3a1d99702e06f422c0b1db9fb7f3c290e
Svetlana <svetlana.maltseva@elastic.co>
2023-05-22 18:11:18 +0200

    ci(buildkite): migration Jenkins pipeline to buildkite. to linux tests (#1003)

---
+++ .buildkite/scripts/lint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+# Pre install:
+source .buildkite/scripts/pre-install-command.sh
+add_bin_path
+with_mage
+
+mage -debug check
---
+++ .buildkite/scripts/run-tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+# Pre install:
+source .buildkite/scripts/pre-install-command.sh
+add_bin_path
+with_mage
+with_go_junit_report
+
+set +e
+mage -debug test | tee tests-report-linux.txt
+exit_code=$?
+
+set -e
+
+# Create Junit report for junit annotation plugin
+go-junit-report > tests-report-linux.xml < tests-report-linux.txt
+exit $exit_code
