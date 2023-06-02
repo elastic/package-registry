2e010577324b7d3d298ec9eae5027ef88056cff5
Svetlana Maltseva <svetlana.maltseva@elastic.co>
2023-06-02 16:43:53 +0200

    change tag for prs

--- .buildkite/scripts/publish.sh
+++ .buildkite/scripts/publish.sh
@@ -6,7 +6,7 @@ source .buildkite/scripts/tooling.sh
 pushDockerImage() {
     docker build \
         -t "${DOCKER_IMG_TAG}" \
-        --label BRANCH_NAME="${GITHUB_PR_BRANCH}" \
+        --label BRANCH_NAME="${TAG_NAME}" \
         --label GIT_SHA="${BUILDKITE_COMMIT}" \
         --label GO_VERSION="${SETUP_GOLANG_VERSION}" \
         --label TIMESTAMP="$(date +%Y-%m-%d_%H:%M)" \
@@ -16,16 +16,17 @@ pushDockerImage() {
     retry 3 docker push "${DOCKER_IMG_TAG_BRANCH}"
 }
 
-if [[ "${BUILDKITE_PULL_REQUEST}" == "false" ]]; then
-    DOCKER_NAMESPACE="${DOCKER_IMG}"
-else
+if [[ -n "${BUILDKITE_PULL_REQUEST:-}" ]]; then
     DOCKER_NAMESPACE="${DOCKER_IMG_PR}"
+    TAG_NAME="PR-${BUILDKITE_PULL_REQUEST}"
+else
+    DOCKER_NAMESPACE="${DOCKER_IMG}"
+    TAG_NAME="${BUILDKITE_BRANCH}"  # e.g. main
 fi
 
-TAG_NAME="${BUILDKITE_BRANCH//\:/-}" # forked branches have names: "<account>:<name-of-the-branch>"
 # if tag exists use tag instead
-if [[ -n ${BUILDKITE_TAG:-} ]]; then
-    TAG_NAME=${BUILDKITE_TAG}
+if [ -n "${BUILDKITE_TAG:-}" ]; then
+    TAG_NAME="${BUILDKITE_TAG}"
 fi
 
 DOCKER_IMG_TAG="${DOCKER_NAMESPACE}:${BUILDKITE_COMMIT}"
