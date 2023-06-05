7d86a884c6d939c2634b6bc58b86a1dfecf49cf7
Svetlana Maltseva <svetlana.maltseva@elastic.co>
2023-06-05 12:43:59 +0200

    for testing: trigger job

--- .buildkite/scripts/trigger-jenkins-job.sh
+++ .buildkite/scripts/trigger-jenkins-job.sh
@@ -3,7 +3,7 @@
 cd .buildkite/scripts/triggerJenkinsJob
 
 go run main.go \
-    --jenkins-job "update-package-registry" \
-    --version "${BUILDKITE_TAG}" \
-    --dry-run "${DRY_RUN}" \
-    --draft "${DRAFT_PR}"
+    --jenkins-job="update-package-registry" \
+    --version="${BUILDKITE_TAG}" \
+    --dry-run="${DRY_RUN}" \
+    --draft="${DRAFT_PR}"
