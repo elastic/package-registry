19cc103613eeb9c1d5dfd26f3e9cdf0c5a4ae38b
Svetlana Maltseva <svetlana.maltseva@elastic.co>
2023-06-09 14:26:01 +0200

    review

--- .buildkite/scripts/validate-tag.sh
+++ .buildkite/scripts/validate-tag.sh
@@ -4,12 +4,13 @@ set -euo pipefail
 source .buildkite/scripts/tooling.sh
 
 transformTagAndValidate() {
-    version=$1
+    local version=$1
     version=${version//\v/}
     if [[ $version =~ ^[0-9]+.[0-9]+.[0-9]+(-[A-Za-z0-9_]+)?$ ]]; then
         echo "valid version: ${version}"
+        DOCKER_TAG_VERSION=${version}
     else
-        echo "unvalid version: ${version}"
+        echo "invalid version: ${version}"
         echo "unsupported docker tag, please use the major.minor.path(-prerelease)? format (for example: 1.2.3 or 1.2.3-alpha)."
         exit 1
     fi
