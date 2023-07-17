82225eb1e3a9b63ac04ea502e9742a293714cb26
Svetlana Maltseva <svetlana.maltseva@elastic.co>
2023-07-17 10:22:55 +0200

    avoid buildkite collapsing

--- .buildkite/scripts/run-tests.sh
+++ .buildkite/scripts/run-tests.sh
@@ -8,10 +8,13 @@ with_mage
 with_go_junit_report
 
 set +e
-mage -debug test | tee tests-report-linux.txt
+mage -debug test > tests-report-linux.txt
 exit_code=$?
-
 set -e
+
+# Buildkite collapse logs under --- symbols
+# need to change --- to anything else or switch off collapsing (note: not available at the moment of this commit)
+awk '{gsub("---", "----"); print }' tests-report-linux.txt
 
 # Create Junit report for junit annotation plugin
 go-junit-report > tests-report-linux.xml < tests-report-linux.txt
