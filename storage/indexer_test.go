a92cada3e1db3dba2b38e77af98c7dc92cca0c35
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2025-04-16 18:44:50 +0200

    Add test case

--- storage/indexer_test.go
+++ storage/indexer_test.go
@@ -220,4 +220,24 @@ func TestGet_IndexUpdated(t *testing.T) {
 	require.Len(t, foundPackages, 1)
 	require.Equal(t, "1password", foundPackages[0].Name)
 	require.Equal(t, "1.4.0", foundPackages[0].Version)
+
+	// when: index update is performed removing packages
+	const thirdRevision = "3"
+	updateFakeServer(t, fs, thirdRevision, "testdata/search-index-all-small.json")
+	err = indexer.updateIndex(context.Background())
+	require.NoError(t, err, "index should be updated successfully")
+
+	foundPackages, err = indexer.Get(context.Background(), &packages.GetOptions{
+		Filter: &packages.Filter{
+			PackageName: "1password",
+			PackageType: "integration",
+			Prerelease:  true,
+		},
+	})
+
+	// then
+	require.NoError(t, err, "packages should be returned")
+	require.Len(t, foundPackages, 1)
+	require.Equal(t, "1password", foundPackages[0].Name)
+	require.Equal(t, "0.2.0", foundPackages[0].Version)
 }
