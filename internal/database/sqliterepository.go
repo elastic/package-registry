7233029d52a0946edfc964e503a12714f3b73c0c
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2025-10-15 15:45:46 +0200

    Remove outdated comment

--- internal/database/sqliterepository.go
+++ internal/database/sqliterepository.go
@@ -496,9 +496,6 @@ type FilterOptions struct {
 	SpecMin       string
 	SpecMax       string
 	Capabilities  []string
-	// It cannot be filtered by capabilities at database level, since it would be
-	// complicated using SQL logic to ensure that all the capabilities defined in the package
-	// are present in the query filter.
 
 	// It cannot be filtered by categories at database level, since
 	// the category filter is applied once all the others have been processed.
