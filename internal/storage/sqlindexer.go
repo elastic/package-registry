5b02281c145d23cdb0c4eabecadd243721a70901
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2025-10-15 13:12:39 +0200

    Allow to query latest packages if capabilities filter is defined
    Capabilities filter is already supported at database level, so it can be
    used the SQL query to return the latest packages for those queries
    setting that filter.

--- internal/storage/sqlindexer.go
+++ internal/storage/sqlindexer.go
@@ -482,7 +482,6 @@ func createDatabaseOptions(cursor string, opts *packages.GetOptions) *database.S
 	}
 
 	// TODO: Add support to filter by discovery fields if possible.
-	// TODO: Add support to filter by capabilities if possible, relates to https://github.com/elastic/package-registry/pull/1396/
 	sqlOptions.Filter = &database.FilterOptions{
 		Type:         opts.Filter.PackageType,
 		Name:         opts.Filter.PackageName,
@@ -502,11 +501,11 @@ func createDatabaseOptions(cursor string, opts *packages.GetOptions) *database.S
 
 	// Determine if we can use the optimized query to get just the latest packages.
 	// We can use it when we are not filtering by version, not requesting all versions,
-	// and not filtering by capabilities or discovery. As capabilities and discovery are not
-	// supported at database level, we can only use the optimized query when they are not set.
-	// If capabilities or discovery filters are added, it needs to be checked that they can be
-	// applied when querying for the latest packages.
-	sqlOptions.JustLatestPackages = !opts.Filter.AllVersions && opts.Filter.PackageVersion == "" && len(opts.Filter.Capabilities) == 0 && opts.Filter.Discovery == nil
+	// and not filtering by discovery. As discovery filter is not supported at database
+	// level, we can only use the optimized query when that is not set.
+	// If discovery filter is added, it needs to be checked that they can be applied when
+	// querying for the latest packages too.
+	sqlOptions.JustLatestPackages = !opts.Filter.AllVersions && opts.Filter.PackageVersion == "" && opts.Filter.Discovery == nil
 
 	return sqlOptions
 }
