27b35cd3689eb91fda0fc0f18cc98c587909cf79
Michael Montgomery <mmontg1@gmail.com>
2025-10-27 15:09:42 -0500

    remove -x
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

--- .buildkite/scripts/publish.sh
+++ .buildkite/scripts/publish.sh
@@ -3,56 +3,54 @@ source .buildkite/scripts/tooling.sh
 
 set -euo pipefail
 
-set -x
-
 build_docker_image() {
-    local go_version
-    local runner_image="${1}"
-    local tag_suffix=""
-    if [ $# -ge 2 ]; then
-        tag_suffix="${2}"
-    fi
-    local docker_img_tag="${DOCKER_IMG_TAG}${tag_suffix}"
-    local docker_img_tag_branch="${DOCKER_IMG_TAG_BRANCH}${tag_suffix}"
-    go_version=$(cat .go-version)
+	local go_version
+	local runner_image="${1}"
+	local tag_suffix=""
+	if [ $# -ge 2 ]; then
+		tag_suffix="${2}"
+	fi
+	local docker_img_tag="${DOCKER_IMG_TAG}${tag_suffix}"
+	local docker_img_tag_branch="${DOCKER_IMG_TAG_BRANCH}${tag_suffix}"
+	go_version=$(cat .go-version)
 
-    docker buildx build --push \
-        --platform linux/amd64,linux/arm64/v8 \
-        --progress plain \
-        -t "${docker_img_tag}" \
-        -t "${docker_img_tag_branch}" \
-        --build-arg GO_VERSION="${go_version}" \
-        --build-arg BUILDER_IMAGE=docker.elastic.co/wolfi/go \
-        --build-arg RUNNER_IMAGE="${runner_image}" \
-        --label BRANCH_NAME="${TAG_NAME}" \
-        --label GIT_SHA="${BUILDKITE_COMMIT}" \
-        --label GO_VERSION="${SETUP_GOLANG_VERSION}" \
-        --label TIMESTAMP="$(date +%Y-%m-%d_%H:%M)" \
-        .
+	docker buildx build --push \
+		--platform linux/amd64,linux/arm64/v8 \
+		--progress plain \
+		-t "${docker_img_tag}" \
+		-t "${docker_img_tag_branch}" \
+		--build-arg GO_VERSION="${go_version}" \
+		--build-arg BUILDER_IMAGE=docker.elastic.co/wolfi/go \
+		--build-arg RUNNER_IMAGE="${runner_image}" \
+		--label BRANCH_NAME="${TAG_NAME}" \
+		--label GIT_SHA="${BUILDKITE_COMMIT}" \
+		--label GO_VERSION="${SETUP_GOLANG_VERSION}" \
+		--label TIMESTAMP="$(date +%Y-%m-%d_%H:%M)" \
+		.
 }
 
 push_docker_image() {
-    local runner_image="${1}"
-    local tag_suffix="${2}"
+	local runner_image="${1}"
+	local tag_suffix="${2}"
 
-    docker buildx create --use
+	docker buildx create --use
 
-    retry 3 build_docker_image ${runner_image} ${tag_suffix}
+	retry 3 build_docker_image ${runner_image} ${tag_suffix}
 
-    echo "Docker images pushed: ${DOCKER_IMG_TAG}${tag_suffix} ${DOCKER_IMG_TAG_BRANCH}${tag_suffix}"
+	echo "Docker images pushed: ${DOCKER_IMG_TAG}${tag_suffix} ${DOCKER_IMG_TAG_BRANCH}${tag_suffix}"
 }
 
 if [[ "${BUILDKITE_PULL_REQUEST}" != "false" ]]; then
-    DOCKER_NAMESPACE="${DOCKER_IMG_PR}"
-    TAG_NAME="PR-${BUILDKITE_PULL_REQUEST}"
+	DOCKER_NAMESPACE="${DOCKER_IMG_PR}"
+	TAG_NAME="PR-${BUILDKITE_PULL_REQUEST}"
 else
-    DOCKER_NAMESPACE="${DOCKER_IMG}"
-    TAG_NAME="${BUILDKITE_BRANCH}"  # e.g. main
+	DOCKER_NAMESPACE="${DOCKER_IMG}"
+	TAG_NAME="${BUILDKITE_BRANCH}" # e.g. main
 fi
 
 # if tag exists use tag instead
 if [ -n "${BUILDKITE_TAG:-}" ]; then
-    TAG_NAME="${BUILDKITE_TAG}"
+	TAG_NAME="${BUILDKITE_TAG}"
 fi
 
 DOCKER_IMG_TAG="${DOCKER_NAMESPACE}:${BUILDKITE_COMMIT}"
