dba6d8a5e4617dd68ae6304f6d7dd7743cb8dda5
Teresa Romero <teresa.romero@elastic.co>
2025-11-21 16:34:38 +0100

    Update indexer initialization to use nil for the second parameter in test files

--- search_test.go
+++ search_test.go
@@ -96,8 +96,8 @@ func TestSearchWithProxyMode(t *testing.T) {
 
 	packagesBasePaths := []string{"./testdata/second_package_path", "./testdata/package"}
 	indexer := NewCombinedIndexer(
-		packages.NewZipFileSystemIndexer(testLogger, false, "./testdata/local-storage"),
-		packages.NewFileSystemIndexer(testLogger, false, packagesBasePaths...),
+		packages.NewZipFileSystemIndexer(testLogger, nil, false, "./testdata/local-storage"),
+		packages.NewFileSystemIndexer(testLogger, nil, false, packagesBasePaths...),
 	)
 	defer indexer.Close(t.Context())
 
