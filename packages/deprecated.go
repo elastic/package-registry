9cd60b87a7b2c6b28a51f91a144e468a74732dfc
Tere <teresa.romero@elastic.co>
2026-01-30 15:28:36 +0100

    Add Deprecated Field Support to Package Registry (#1516)

---
+++ packages/deprecated.go
@@ -0,0 +1,54 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+package packages
+
+import "github.com/Masterminds/semver/v3"
+
+type deprecatedMeta struct {
+	deprecated *Deprecated
+	version    *semver.Version
+}
+
+type DeprecatedPackages map[string]deprecatedMeta
+
+func (d DeprecatedPackages) Deprecated(name string) (*Deprecated, bool) {
+	meta, found := d[name]
+	if !found {
+		return nil, false
+	}
+	return meta.deprecated, true
+}
+
+// UpdateLatestDeprecatedPackagesMapByName updates a map of the latest deprecated packages by name.
+// It ensures that for each package name, only the deprecation info of the latest version is stored.
+func UpdateLatestDeprecatedPackagesMapByName(input Packages, deprecatedPackages DeprecatedPackages) {
+	for _, pkg := range input {
+
+		if pkg.BasePackage.Deprecated == nil {
+			continue
+		}
+		deprecated := pkg.BasePackage.Deprecated
+
+		// if not existing or current version is greater than existing, update
+		existing, found := deprecatedPackages[pkg.BasePackage.Name]
+		if !found || pkg.versionSemVer.GreaterThan(existing.version) {
+			deprecatedPackages[pkg.BasePackage.Name] = deprecatedMeta{
+				deprecated: deprecated,
+				version:    pkg.versionSemVer,
+			}
+		}
+
+	}
+}
+
+// PropagateLatestDeprecatedInfoToPackageList adds deprecation information to all packages in the package list
+// based on the latest deprecated info available in the deprecated packages map.
+func PropagateLatestDeprecatedInfoToPackageList(packageList Packages, deprecatedPackages DeprecatedPackages) {
+	for _, pkg := range packageList {
+		if deprecatedInfo, found := deprecatedPackages.Deprecated(pkg.Name); found {
+			pkg.Deprecated = deprecatedInfo
+		}
+	}
+}
