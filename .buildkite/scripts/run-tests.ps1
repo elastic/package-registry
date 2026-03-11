a4d46d826cbea165494caacc8d362ffa0d4e78a9
Tere <teresa.romero@elastic.co>
2026-03-11 11:31:27 +0100

    fix latest_1_25_versions

--- .buildkite/scripts/run-tests.ps1
+++ .buildkite/scripts/run-tests.ps1
@@ -10,7 +10,7 @@ function fixCRLF {
 
 function withGolang($version) {
     Write-Host "--- Install golang"
-    $latest_1_25_versions = @('1.25.7')
+    $latest_1_25_versions = @('1.25.8')
     # not all latest Golang versions of 1.25.x are available since 1.26.0 is released
     if ($version -in $latest_1_25_versions) {
         Write-Host "$version not available in cholocatey, using 1.25.7"
